5d5c535dda5ac9ab1b9b7a870fab758afac4020e
Kieran <kiermccarth+github@gmail.com>
2023-05-04 09:53:30 +0100

    Added unit test

--- __test__/auth.test.ts
+++ __test__/auth.test.ts
@@ -2,34 +2,51 @@
 /*
  * At the end of testing, a 404 error is returned referencing api/_supabase/session
  * Doesn't appear to affect tests as all execute with no issues
- * Need to look further into why
+ * Have searched+ for a response but nothing concrete exists
+ * Edited @nuxtjs/supabase/dist/runtime/plugins/supabase.client.mjs with a try/catch block to quiet the issue
+ * No observable difference in the rest of the app
  */
+/* Type imports */
+import type { Store } from "pinia";
 /* Vitest imports */
-import { vi, describe, test, expect, beforeEach, afterEach } from "vitest";
+import {
+  vi,
+  describe,
+  test,
+  expect,
+  beforeAll,
+  afterAll,
+  beforeEach,
+  afterEach,
+} from "vitest";
 /* Pinia imports */
 import { setActivePinia, createPinia } from "pinia";
 /* Functions to be tested */
 import { loginUser, createUser, logoutUser } from "~/composables/auth";
 
 /* Module/function mocking */
-const signInWithPasswordMock = vi.fn();
+const mocked = vi.fn();
 vi.mock("@nuxtjs/supabase");
-/* Datatype mocking */
-const loginCredentials: LoginCredentialsDataObject = reactive({
-  email: "",
-  password: "",
+
+// Create a new Pinia instance
+beforeAll(() => {
+  setActivePinia(createPinia());
+});
+// Reset all mock values and the notifications store
+afterAll(() => {
+  useNotificationsStore().$reset;
+  vi.resetAllMocks();
 });
 
 describe("Tests related to logging in", () => {
-  // Create a new Pinia instance and reset the loginCredentials object
-  beforeEach(() => {
-    setActivePinia(createPinia());
+  const loginCredentials: LoginCredentialsDataObject = reactive({
+    email: "",
+    password: "",
+  });
+  // Reset credentials to empty strings
+  afterEach(() => {
     loginCredentials.email = "";
     loginCredentials.password = "";
-  });
-  // Reset all mock values after each test
-  afterEach(() => {
-    vi.resetAllMocks();
   });
   /*
    * Calls auth.ts > loginUser()
@@ -42,7 +59,7 @@ describe("Tests related to logging in", () => {
     // Mock a fake error message and intercept the auth client login function
     const mock = { message: "Invalid login credentials" };
     useSupabaseAuthClient().auth.signInWithPassword =
-      signInWithPasswordMock.mockResolvedValueOnce({ error: mock });
+      mocked.mockResolvedValueOnce({ error: mock });
     // Call the loginUser function as it is used and create assertions
     await loginUser(ref(loginCredentials));
     expect(notifications.message).toBe("Invalid login credentials");
@@ -64,7 +81,7 @@ describe("Tests related to logging in", () => {
       user: { email: "validemail@domain.com", user_metadata: { name: "" } },
     };
     useSupabaseAuthClient().auth.signInWithPassword =
-      signInWithPasswordMock.mockResolvedValueOnce({ data: mock });
+      mocked.mockResolvedValueOnce({ data: mock });
     // Call the loginUser function as it is used and create assertions
     await loginUser(ref(loginCredentials));
     expect(notifications.message).toBe("Logged in as validemail@domain.com");
@@ -73,10 +90,36 @@ describe("Tests related to logging in", () => {
     vi.resetAllMocks();
     mock.user.user_metadata.name = "User";
     useSupabaseAuthClient().auth.signInWithPassword =
-      signInWithPasswordMock.mockResolvedValueOnce({ data: mock });
+      mocked.mockResolvedValueOnce({ data: mock });
     // Call the loginUser function as it is used and create assertions
     await loginUser(ref(loginCredentials));
     expect(notifications.message).toBe("Logged in as User");
     expect(notifications.type).toBe("success");
+  });
+});
+
+describe("Tests related to creating an account", () => {
+  const newUserCredentials: NewAccountDataObject = reactive({
+    email: "",
+    password: "",
+    name: "",
+  });
+  afterEach(() => {
+    newUserCredentials.email = "";
+    newUserCredentials.password = "";
+    newUserCredentials.name = "";
+  });
+  test("Using empty credentials should push an error message as a notification", async () => {
+    // Instantiate the notification store
+    const notifications = useNotificationsStore();
+    // Mock a fake error message and intercept the auth client create account function
+    const mock = { message: "Signup requires a valid password" };
+    useSupabaseAuthClient().auth.signUp = mocked.mockResolvedValueOnce({
+      error: mock,
+    });
+    // Call the createUser function as it is used and create assertions
+    await createUser(ref(newUserCredentials));
+    expect(notifications.message).toBe("Signup requires a valid password");
+    expect(notifications.type).toBe("error");
   });
 });
