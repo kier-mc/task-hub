13654daa7ef8b5518593116192e7f52c32e63dd7
Kieran <kiermccarth+github@gmail.com>
2023-05-26 21:19:57 +0100

    Updated to make use of altered logic/new icon set

--- components/WeatherDisplay.vue
+++ components/WeatherDisplay.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="weather-applet" v-if="weatherStore.weather">
+  <div class="weather-applet" v-if="weatherStore.data">
     <div class="weather-applet__left">
       <div class="weather-applet__location">
-        {{ weatherStore.weather.name }}, {{ weatherStore.weather.sys.country }}
+        {{ weatherStore.data.name }}, {{ weatherStore.data.sys.country }}
       </div>
       <div class="weather-applet__date">
         {{ currentDay }}, {{ currentDate }}
+      </div>
+    </div>
+    <div class="weather-applet__right">
+      <img
+        class="weather-applet__icon"
+        :alt="weatherStore.data.weather[0].description"
+        :src="`/img/svg/weather/${weatherStore.data.weather[0].icon}.svg`"
+      />
+
+      <div class="weather-applet__temperature">
+        <span class="weather-applet__temperature--value">
+          {{ weatherStore.getTemperature("celsius") }}
+        </span>
+        <span class="weather-applet__temperature--symbol">&nbsp;°C</span>
       </div>
     </div>
   </div>
@@ -17,6 +31,23 @@
   grid-template-columns: repeat(2, 1fr);
   &__location {
     font-size: 2rem;
+  }
+  &__icon {
+    aspect-ratio: 1/1;
+    max-width: 128px;
+  }
+  &__right {
+    display: flex;
+    align-items: center;
+  }
+  &__temperature {
+    &--value {
+      font-size: 4.25rem;
+      letter-spacing: -0.25rem;
+    }
+    &--symbol {
+      font-size: 1.5rem;
+    }
   }
 }
 </style>
@@ -30,9 +61,10 @@ const currentDay = ref("");
 const currentDate = ref("");
 onMounted(async () => {
   await weatherStore.getWeather(defaultLocation);
-  currentDay.value = convertDate(currentDateTime, "en-GB", {
-    weekday: "long",
-  }) as string;
-  currentDate.value = convertDate(currentDateTime, "en-GB") as string;
+  currentDay.value =
+    convertDate(currentDateTime, "en-GB", {
+      weekday: "long",
+    }) ?? "";
+  currentDate.value = convertDate(currentDateTime, "en-GB") ?? "";
 });
 </script>
