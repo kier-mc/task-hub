710573b5c38cf0e94e5bc8b1de08650838ac93c1
Kieran <kiermccarth+github@gmail.com>
2023-06-12 17:49:44 +0100

    Implemented loading indicator for task list component

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,25 +1,56 @@
 <template>
   <div class="view-task">
-    <template v-if="!taskStore.taskCount()"> No tasks found </template>
+    <div v-if="isLoading" class="view-task__loading">
+      <AppLoadingIndicator :options="propData.loadingIndicator" />
+    </div>
     <template v-else>
-      <div class="tasks" v-for="(task, index) in taskStore.tasks" :key="index">
-        <TaskItem :taskData="task" />
-      </div>
+      <template v-if="!taskStore.taskCount()">No tasks found!</template>
+      <template v-else>
+        <div
+          class="view-task__tasks"
+          v-for="(task, index) in taskStore.tasks"
+          :key="index"
+        >
+          <TaskItem :taskData="task" />
+        </div>
+      </template>
     </template>
   </div>
 </template>
 
 <style scoped lang="scss">
-.tasks {
-  &:not(:last-child) {
-    margin-bottom: 1rem;
+.view-task {
+  &__loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &__tasks {
+    &:not(:last-child) {
+      margin-bottom: 1.5rem;
+    }
   }
 }
 </style>
 
 <script setup lang="ts">
+/* Pinia stores */
 const taskStore = useTaskStore();
+/* Prop data */
+const propData = {
+  loadingIndicator: {
+    type: "dots",
+    width: 48,
+    height: 48,
+    hue: 0,
+    saturation: 0,
+    lightness: 100,
+  } as LoadingIndicatorDataObject,
+};
+/* Reactive variables */
+const isLoading: Ref<boolean> = ref(true);
 onMounted(async () => {
-  if (!taskStore.tasks) await taskStore.getTasks();
+  if (!taskStore.taskCount()) await taskStore.getTasks();
+  isLoading.value = false;
 });
 </script>
