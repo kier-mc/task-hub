c4416537245ada64e55994246fffb5f101951bd0
Kieran <kiermccarth+github@gmail.com>
2023-07-05 14:57:03 +0100

    Changed style to Material Design

--- components/svg/SVGXMark.vue
+++ components/svg/SVGXMark.vue
@@ -1,7 +1,8 @@
 <template>
-    <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-    <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+    <path
+      d="m250.923-218.924-31.999-31.999L448.001-480 218.924-709.077l31.999-31.999L480-511.999l229.077-229.077 31.999 31.999L511.999-480l229.077 229.077-31.999 31.999L480-448.001 250.923-218.924Z"
+    />
   </svg>
 </template>
 <style scoped lang="scss"></style>
