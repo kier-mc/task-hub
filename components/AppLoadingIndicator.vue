9d85afb55535bd4c88a3253321c05f1331be821f
Kieran <kiermccarth+github@gmail.com>
2023-07-14 12:37:49 +0100

    Updated component to remove redundant props

--- components/AppLoadingIndicator.vue
+++ components/AppLoadingIndicator.vue
@@ -4,17 +4,7 @@
  -->
 <template>
   <template v-if="options.type === 'circle'">
-    <svg
-      :width="options.size.width"
-      :height="options.size.height"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      :fill="`hsl(
-      ${options.colour.hue},
-      ${options.colour.saturation}%,
-      ${options.colour.lightness}%)
-    `"
-    >
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
       >
@@ -29,17 +19,7 @@
     </svg>
   </template>
   <template v-else-if="options.type === 'dots'">
-    <svg
-      :width="options.size.width"
-      :height="options.size.height"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      :fill="`hsl(
-      ${options.colour.hue},
-      ${options.colour.saturation}%,
-      ${options.colour.lightness}%)
-    `"
-    >
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <circle cx="4" cy="12" r="3" opacity="1">
         <animate
           id="spinner_qYjJ"
