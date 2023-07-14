8a66b570c4dee6c9ce615c45bb7c96628a611791
Kieran <kiermccarth+github@gmail.com>
2023-07-14 12:36:49 +0100

    Updated app header, split component to separate header/navigation menu and optimised code for readability

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,264 +1,64 @@
 <template>
   <header class="header">
-    <h1 class="header-title">
-      <NuxtLink to="/">To-Do</NuxtLink>
-    </h1>
+    <h1 class="header__title">Task Hub</h1>
     <button
-      type="button"
-      class="header__menu-button"
-      aria-controls="nav__menu"
-      :aria-expanded="menuIsOpen ? 'true' : 'false'"
-      @click="menuIsOpen = !menuIsOpen"
-      ref="button"
+      class="header__button"
+      id="app-navigation-button"
+      aria-controls="app-navigation-menu"
+      @click="isExpanded = !isExpanded"
     >
-      <SVGMenu class="header__menu-icon" />
+      <SVGMenu class="header__icon" />
     </button>
   </header>
-  <nav class="nav" role="navigation">
-    <div
-      id="nav__menu"
-      class="nav__menu"
-      :class="{ 'nav__menu--visible': menuIsOpen }"
-    >
-      <!-- Menu if user is logged in -->
-      <template v-if="user">
-        <template v-for="data in propData.navData" :key="data.index">
-          <section class="nav__section" v-if="data.display !== 'auth=false'">
-            <h1 class="nav__title">{{ data.title }}</h1>
-            <ul class="nav__links">
-              <template v-for="link in data.links" :key="link.index">
-                <template v-if="link.display !== 'auth=false'">
-                  <template v-if="link.type === 'route'">
-                    <li class="nav__link" @click="navigateTo(link.url)">
-                      {{ link.name }}
-                    </li>
-                  </template>
-                  <template v-else-if="link.type === 'logout'">
-                    <li class="nav__link" @click="logoutUser">
-                      {{ link.name }}
-                    </li>
-                  </template>
-                </template>
-              </template>
-            </ul>
-          </section>
-        </template>
-      </template>
-      <!-- Menu if user is not logged in -->
-      <template v-else>
-        <template v-for="data in propData.navData" :key="data.index">
-          <section
-            class="nav__section"
-            v-if="data.display === 'auth=false' || data.display === 'always'"
-          >
-            <h1 class="nav__title">{{ data.title }}</h1>
-            <ul class="nav__links">
-              <template v-for="link in data.links" :key="link.index">
-                <template
-                  v-if="
-                    link.display === 'auth=false' || link.display === 'always'
-                  "
-                >
-                  <template v-if="link.type === 'route'">
-                    <li class="nav__link" @click="navigateTo(link.url)">
-                      {{ link.name }}
-                    </li>
-                  </template>
-                </template>
-              </template>
-            </ul>
-          </section>
-        </template>
-      </template>
-    </div>
-  </nav>
+  <AppNavigation id="app-navigation-menu" :is-expanded="isExpanded" />
 </template>
 
 <style scoped lang="scss">
-@import "../assets/scss/variables.scss";
+@use "../assets/scss/data/colour";
+@use "../assets/scss/data/layout";
 .header {
-  isolation: isolate;
-  position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  max-width: $breakpoint-largest;
-  z-index: 30;
+  max-width: layout.$breakpoint-largest;
+  height: inherit;
   padding-inline: 1rem;
-  margin: 0 auto;
-  &__menu-button {
+  margin-inline: auto;
+  &__title {
+    all: unset;
+    color: colour.$font-light;
+    font-size: calc(1rem * 1.25);
+    letter-spacing: 0.075rem;
+  }
+  &__button {
     all: unset;
     display: flex;
     justify-content: center;
     align-items: center;
     aspect-ratio: 1/1;
-    min-width: 48px;
-    z-index: 20;
-    transition: background-color 200ms;
+    width: 3rem;
     cursor: pointer;
-    &[aria-expanded="true"] .header__menu-icon {
-      fill: hsl(0, 0%, 90%);
-    }
   }
-  &__menu-icon {
+  &__icon {
     pointer-events: none;
-    z-index: 0;
-    max-width: 32px;
-    fill: hsl(0, 0%, 80%);
+    width: 1.5rem;
+    fill: colour.$font-light;
     transition: fill 125ms;
     &:hover {
       fill: hsl(0, 0%, 90%);
-    }
-  }
-  & a {
-    all: unset;
-    cursor: pointer;
-  }
-}
-.nav {
-  position: relative;
-  max-width: $breakpoint-largest;
-  margin: 0 auto;
-  user-select: none;
-  /* prettier-ignore */
-  &__menu {
-    opacity: 0;
-    transform: translate3d(0, -100%, 0);
-    filter: blur($blur-default);
-    position: absolute;
-    top: 0;
-    right: 0;
-    display: flex;
-    column-gap: 5rem;
-    z-index: -10;
-    padding: 0 1rem 1rem 1rem;
-    background-color: $colour-lm-nav-menu;
-    color: $colour-lm-font;
-    backdrop-filter: blur($blur-default);
-    transition:
-      opacity $transition-short,
-      transform $transition-long,
-      filter $transition-short;
-    @media (max-width: $breakpoint-small) {
-      flex-direction: column;
-      width: 100%;
-    }
-    @media (max-width: $breakpoint-large) {
-      justify-content: space-evenly;
-      width: 100%;
-    }
-    @media (prefers-color-scheme: dark) {
-      background-color: $colour-dm-nav-menu;
-      color: $colour-dm-font;
-    }
-    &--visible {
-      opacity: 1;
-      transform: translate3d(0, 0%, 0);
-      filter: none;
-    }
-  }
-  &__title {
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    padding-left: 0.75rem;
-    font-size: $font-size-largest;
-  }
-  &__links {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    list-style-type: none;
-    font-size: $font-size-small;
-    cursor: default;
-  }
-  &__link {
-    width: 25ch;
-    padding: 1rem;
-    border: 1px solid $colour-lm-border;
-    cursor: pointer;
-    transition: border $transition-medium;
-    @media (max-width: $breakpoint-small) {
-      width: 100%;
-    }
-    @media (prefers-color-scheme: dark) {
-      border: 1px solid $colour-dm-border;
-    }
-    &:hover {
-      border: 1px solid $colour-primary;
     }
   }
 }
 </style>
 
 <script setup lang="ts">
-const menuIsOpen: Ref<boolean> = ref(false);
-const button: Ref<HTMLElement | null> = ref(null);
-const user = useSupabaseUser();
-const propData = {
-  navData: [
-    {
-      index: 0,
-      title: "Site",
-      display: "always",
-      links: [
-        {
-          index: 0,
-          name: "Home",
-          type: "route",
-          display: "always",
-          url: "/",
-        },
-        {
-          index: 1,
-          name: "Login",
-          type: "route",
-          display: "auth=false",
-          url: "/login",
-        },
-        {
-          index: 2,
-          name: "Create Account",
-          type: "route",
-          display: "auth=false",
-          url: "/create-account",
-        },
-        {
-          index: 3,
-          name: "Hub",
-          type: "route",
-          display: "auth=true",
-          url: "/hub",
-        },
-        {
-          index: 4,
-          name: "Logout",
-          type: "logout",
-          display: "auth=true",
-        },
-      ],
-    } as NavigationData,
-    {
-      index: 1,
-      title: "User",
-      display: "auth=true",
-      links: [
-        {
-          index: 0,
-          name: "Settings",
-          type: "route",
-          display: "auth=true",
-          url: "/settings",
-        },
-      ],
-    } as NavigationData,
-  ],
-};
+const isExpanded: Ref<boolean> = ref(false);
 
 function closeMenuWithClickOutside(event: Event): void {
   const target = event.target as Element;
-  const isClickInside = target.closest(".header__menu-button");
+  const isClickInside = target.closest("#app-navigation-button");
   if (!isClickInside) {
-    menuIsOpen.value = false;
+    isExpanded.value = false;
   }
 }
 
---
+++ components/AppNavigation.vue
@@ -0,0 +1,156 @@
+<template>
+  <nav class="nav" role="navigation">
+    <ul class="nav__menu" :aria-expanded="props.isExpanded">
+      <template v-for="(option, index) in propData.navigation" :key="index">
+        <template v-if="user && option.requiresAuth === true">
+          <li class="nav__option" @click="option.function">
+            <div class="option__label">{{ option.label }}</div>
+            <div class="option__icon">
+              <component class="option__icon" :is="option.icon" />
+            </div>
+          </li>
+        </template>
+        <template v-else-if="!user && option.requiresAuth === false">
+          <li class="nav__option" @click="option.function">
+            <div class="option__label">{{ option.label }}</div>
+            <div class="option__icon">
+              <component class="option__icon" :is="option.icon" />
+            </div>
+          </li>
+        </template>
+      </template>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/scss/data/colour";
+@use "../assets/scss/data/effect";
+@use "../assets/scss/data/layout";
+.nav {
+  position: relative;
+  max-width: layout.$breakpoint-largest;
+  margin: 0 auto;
+  user-select: none;
+  /* prettier-ignore */
+  &__menu {
+    all: unset;
+    overflow: hidden;
+    visibility: hidden;
+    height: 0;
+    position: absolute;
+    top: 0;
+    right: 0;    
+    background-color: colour.$app-navigation-menu;
+    box-shadow: effect.$drop-shadow-3-lighter;
+    color: colour.$font-light;
+    transition:
+      visibility 200ms ease-in-out,
+      height 175ms ease-in-out;
+    &[aria-expanded="true"] {
+      visibility: visible;
+      height: v-bind(menuHeight);
+      z-index: 990;
+    }
+    &[aria-expanded="true"] .nav__option {
+      opacity: 1;
+      z-index: 1000;
+    }
+  }
+  /* prettier-ignore */
+  &__option {
+    all: unset;
+    opacity: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 3rem;
+    width: 15rem;
+    padding-inline: 1rem;
+    cursor: pointer;
+    transition:
+      background-color 125ms ease-in-out,
+      opacity 150ms ease-in-out 100ms;
+    &:hover {
+      background-color: colour.$app-navigation-menu-hover;
+    }
+  }
+}
+.option {
+  &__label {
+    font-weight: bold;
+  }
+  &__icon {
+    aspect-ratio: 1/1;
+    width: 1.5rem;
+    fill: colour.$font-light;
+  }
+}
+</style>
+
+<script setup lang="ts">
+import {
+  SVGHome,
+  SVGCreateAccount,
+  SVGLogin,
+  SVGSettings,
+  SVGLogout,
+} from "#components";
+
+const props = defineProps({
+  isExpanded: { type: Boolean as PropType<boolean>, required: true },
+});
+
+const user = useSupabaseUser();
+
+const propData = {
+  navigation: [
+    {
+      label: "Home",
+      icon: SVGHome,
+      function: () => navigateTo("/"),
+      requiresAuth: false,
+    },
+    {
+      label: "Hub",
+      icon: SVGHome,
+      function: () => navigateTo("/hub"),
+      requiresAuth: true,
+    },
+    {
+      label: "Create Account",
+      icon: SVGCreateAccount,
+      function: () => navigateTo("/create-account"),
+      requiresAuth: false,
+    },
+    {
+      label: "Login",
+      icon: SVGLogin,
+      function: () => navigateTo("/login"),
+      requiresAuth: false,
+    },
+    {
+      label: "Settings",
+      icon: SVGSettings,
+      function: () => navigateTo("/settings"),
+      requiresAuth: true,
+    },
+    {
+      label: "Logout",
+      icon: SVGLogout,
+      function: () => logoutUser(),
+      requiresAuth: true,
+    },
+  ],
+};
+
+const menuHeight = computed(() => {
+  const count = propData.navigation.reduce((accumulator, option) => {
+    if ((user && option.requiresAuth) || (!user && !option.requiresAuth)) {
+      return accumulator + 1;
+    }
+    return accumulator;
+  }, 0);
+  return `${count * 3}rem`;
+});
+</script>
