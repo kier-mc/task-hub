b9a374904343eb8493791a7be7b22b3a6dc19b15
Kieran <kiermccarth+github@gmail.com>
2023-07-14 12:35:32 +0100

    Updated styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,24 +9,15 @@
 </template>
 
 <style scoped lang="scss">
-@import "../assets/scss/variables.scss";
+@use "../assets/scss/data/colour.scss";
+@use "../assets/scss/data/layout.scss";
 .container-header {
-  isolation: isolate;
-  position: fixed;
-  top: 0;
-  height: $header-height;
-  width: 100%;
-  z-index: 990;
-  background-color: $colour-lm-header;
-  backdrop-filter: blur($blur-default);
-  @media (prefers-color-scheme: dark) {
-    background-color: $colour-dm-header;
-  }
+  height: 3rem;
+  background-image: colour.$app-header;
 }
 .container-main {
-  max-width: $breakpoint-largest;
+  max-width: layout.$breakpoint-largest;
   margin: 0 auto;
-  margin-top: calc($header-height + $content-offset);
 }
 </style>
 
