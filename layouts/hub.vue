e039ec6310ebbfad0ed7a58015efd6fce8ce9945
Kieran <kiermccarth+github@gmail.com>
2023-07-22 16:40:17 +0100

    Added new layout template for hub route

---
+++ layouts/hub.vue
@@ -0,0 +1,20 @@
+<template>
+  <ToastSystem />
+  <div class="container-header">
+    <AppHeader />
+  </div>
+  <main class="container-main">
+    <slot />
+  </main>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/scss/data/colour.scss";
+@use "../assets/scss/data/layout.scss";
+.container-header {
+  height: 3rem;
+  background-image: colour.$app-header;
+}
+</style>
+
+<script setup lang="ts"></script>
