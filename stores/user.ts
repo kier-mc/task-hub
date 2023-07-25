b301aab8edf509f6f5a012dc88d03e9164b98c70
Kieran <kiermccarth+github@gmail.com>
2023-07-25 15:29:45 +0100

    Significant rework of user store logic; improved fetching logic, stricter error handling and more efficient store methods

--- stores/user.ts
+++ stores/user.ts
@@ -1,5 +1,12 @@
-/* Type definitions */
-import { User } from "@supabase/supabase-js";
+/* Type imports */
+import type { UserStoreState } from "types/store.user";
+import type { Database, AppUserMetadata } from "types/schema";
+import type { TemperatureUnitsShort } from "types/unions/generic.units";
+import type {
+  CountryID,
+  CountryName,
+  CountryISOCode,
+} from "types/unions/schema.country";
 
 /**
  * A Pinia store for handling user data.
@@ -7,68 +14,97 @@ import { User } from "@supabase/supabase-js";
 export const useUserStore = defineStore("user", {
   state: (): UserStoreState => ({
     data: null,
-    unit: "celsius",
+    name: null,
+    country: {
+      country_id: null,
+      name: null,
+      iso_code: null,
+      locale: null,
+    },
+    preferences: {
+      units: {
+        temperature: null ?? "c",
+      },
+    },
   }),
   actions: {
-    async fetchData(): Promise<User | void> {
+    async fetchData(force?: boolean): Promise<void> {
+      if (!force && this.data) return;
       const request = await useSupabaseAuthClient().auth.getUser();
-      if (!request.data.user) return;
-      return (this.data = request.data.user);
-    },
-    clearData(): void {
-      this.data = null;
-    },
-    getPreferredName(): string | void {
-      if (!this.data) return;
-      return this.data.user_metadata.preferred_name;
-    },
-    getCountryID(): number | void {
-      if (!this.data) return;
-      return this.data.user_metadata.country_id;
-    },
-    getCountryName(): CountryName | void {
-      if (!this.data) return;
-      const country = convertCountry(
-        this.data.user_metadata.country_id
-      ) as CountryName;
-      return country;
-    },
-    getCountryISOCode(): CountryISOCode | void {
-      if (!this.data) return;
-      const code = getISOCodeFromCountry(this.getCountryName() as CountryName);
-      return code;
-    },
-    getLocale(): string | void {
-      if (!this.data) return;
-      return this.data.user_metadata.locale;
-    },
-    getPreferredUnit(): UnitPreferenceObject {
-      switch (this.unit) {
-        case "celsius": {
-          return {
-            unit: "celsius",
-            symbol: "°C",
-          };
-        }
-        case "kelvin": {
-          return {
-            unit: "kelvin",
-            symbol: "K",
-          };
-        }
-        case "fahrenheit": {
-          return {
-            unit: "fahrenheit",
-            symbol: "°F",
-          };
-        }
-        default: {
-          return {
-            unit: "celsius",
-            symbol: "°C",
-          };
-        }
+      if (!request.data.user) {
+        throw new Error(
+          "Unable to fetch data from remote server. Check your connection and ensure that you are logged in"
+        );
       }
+      this.data = request.data.user;
+      const { data, error } = await useSupabaseClient<Database>()
+        .from("users")
+        .select(
+          `
+          preferred_name,
+          country_id,
+          locale,
+          preferences_units,
+          preferences_app
+          `
+        );
+      if (error) {
+        throw new Error(error.message);
+      }
+      if (data) {
+        const response: AppUserMetadata = data[0];
+        [
+          this.name,
+          this.country.country_id,
+          this.country.name,
+          this.country.iso_code,
+          this.country.locale,
+          this.preferences.units.temperature,
+        ] = [
+          response.preferred_name,
+          response.country_id,
+          countryData.searchByID(response.country_id).name,
+          countryData.searchByID(response.country_id).iso_code,
+          response.locale,
+          response.preferences_units.temperature,
+        ];
+      }
+    },
+    getName(): string | null {
+      if (!this.data) {
+        throw new Error("No local user data available");
+      }
+      return this.name;
+    },
+    getCountryID(): CountryID | null {
+      if (!this.data) {
+        throw new Error("No local user data available");
+      }
+      return this.country.country_id;
+    },
+    getCountryName(): CountryName | null {
+      if (!this.data) {
+        throw new Error("No local user data available");
+      }
+      return this.country.name;
+    },
+    getCountryISOCode(): CountryISOCode | null {
+      if (!this.data) {
+        throw new Error("No local user data available");
+      }
+      return this.country.iso_code;
+    },
+    getLocale(): string | null {
+      if (!this.data) {
+        throw new Error("No local user data available");
+      }
+      return this.country.locale;
+    },
+    getPreferredUnit(): TemperatureUnitsShort | null {
+      if (!this.data) {
+        throw new Error("No local user data available");
+      }
+      return this.preferences.units.temperature;
     },
   },
 });
