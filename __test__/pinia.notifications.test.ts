8268d72226fe97dbd067ddd7cb4e7b5bc95f0a8b
Kieran <kiermccarth+github@gmail.com>
2023-07-27 20:06:59 +0100

    Updated tests to work with new notification store state variables

--- __test__/pinia.notifications.test.ts
+++ __test__/pinia.notifications.test.ts
@@ -13,30 +13,23 @@ describe("Tests related to notification store default state", () => {
   beforeEach(() => {
     useNotificationsStore().$reset;
   });
+  test("When the store is instantiated, 'title' should be null", () => {
+    const notificationsStore = useNotificationsStore();
+    expect(notificationsStore.title).toBe(null);
+  });
   test("When the store is instantiated, 'message' should be null", () => {
     const notificationsStore = useNotificationsStore();
     expect(notificationsStore.message).toBe(null);
-  });
-  test("When the store is instantiated, 'type' should be null", () => {
-    const notificationsStore = useNotificationsStore();
-    expect(notificationsStore.type).toBe(null);
   });
 });
 describe("Tests related to notification store methods", () => {
   beforeEach(() => {
     useNotificationsStore().$reset;
   });
-  test("The setMessage method should update the stored message and type with the specified parameters", () => {
+  test("The push method should update the state title and message with the specified parameters", () => {
     const notificationsStore = useNotificationsStore();
-    notificationsStore.setMessage("This is a test message", "test");
+    notificationsStore.push("Test", "This is a test message");
+    expect(notificationsStore.title).toBe("Test");
     expect(notificationsStore.message).toBe("This is a test message");
-    expect(notificationsStore.type).toBe("test");
-  });
-  test("The clearAll method should reset 'message' and 'type' to null", () => {
-    const notificationsStore = useNotificationsStore();
-    notificationsStore.setMessage("This is a test message", "test");
-    notificationsStore.clearAll();
-    expect(notificationsStore.message).toBe(null);
-    expect(notificationsStore.type).toBe(null);
   });
 });
