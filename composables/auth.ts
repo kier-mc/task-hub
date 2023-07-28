e7e10c5cb6fed12317920729e03602f4d5cbdd4e
Kieran <kiermccarth+github@gmail.com>
2023-07-28 17:30:10 +0100

    Updated types, added stricter error handling and updated data structure for credential objects

--- composables/auth.ts
+++ composables/auth.ts
@@ -1,120 +1,95 @@
-/*
- * AUTH.TS
- * Contains functions related to user auth i.e login, logout, create account etc.
- * Hashing not required: https://github.com/orgs/supabase/discussions/437
- * notificationsStore will fail if declared globally as it will init prior to Pinia
- * Declared at start of each function instead, which can only be called after init
- */
+// Types
+import type {
+  LoginCredentialData,
+  NewAccountCredentialData,
+} from "types/credentials";
 
 /**
  * Attempts login via SupabaseAuthClient (@nuxtjs/supabase).
- * Pushes a notification to the user and redirects to hub page.
- * @param credentials {Ref<LoginCredentialsData>}
+ * Pushes a notification to the user and returns a boolean to indicate whether the request was successful.
+ * @param credentials {Ref<LoginCredentialData>}
  * Object containing data (username, password) to pass to the back end.
+ * @returns {Promise<boolean>} A boolean that determines whether the login attempt was successful or not.
  */
 export async function loginUser(
-  credentials: Ref<LoginCredentialsData>
-): Promise<void> {
+  credentials: Ref<LoginCredentialData>
+): Promise<boolean> {
   const notificationsStore = useNotificationsStore();
   if (!allFieldsArePopulated(credentials)) {
-    notificationsStore.setMessage(
-      "Login failed. Please ensure all fields are filled in.",
-      "error"
+    notificationsStore.push(
+      "Error",
+      "Login failed. Please ensure all fields are filled in."
     );
-    return;
+    return false;
   }
   const userStore = useUserStore();
   const { data, error } = await useSupabaseAuthClient().auth.signInWithPassword(
     {
-      email: credentials.value.email as string,
-      password: credentials.value.password as string,
+      email: credentials.value.email!,
+      password: credentials.value.password!,
     }
   );
   if (error) {
-    notificationsStore.setMessage(error.message, "error");
-    return;
+    notificationsStore.push("Error", error.message);
+    return false;
   }
   if (data.user) {
-    notificationsStore.setMessage(
-      `Logged in as ${
-        data.user.user_metadata.preferred_name
-          ? data.user.user_metadata.preferred_name
-          : data.user.email
-      }!`,
-      "success"
-    );
     await userStore.fetchData();
+    notificationsStore.push("Success", `Logged in as ${userStore.getName()}!`);
     clearAllFields(credentials);
-    await navigateTo("/hub");
+    return true;
   }
+  return false;
 }
 /**
  * Attempts account creation via SupabaseAuthClient (@nuxtjs/supabase).
- * Pushes a notification to the user and redirects to login page.
+ * Pushes a notification to the user and returns a boolean to indicate whether the request was successful.
  * Credentials are cleared where the function is called, as it accepts a
  * computed ref as a parameter and only its constituents can be modified.
  * @param credentials {ComputedRef<CompleteNewAccountCredentialData>}
  * Object containing data to pass to the back end.
  */
 export async function createUser(
-  credentials: ComputedRef<CompleteNewAccountCredentialData>
-) {
+  credentials: Ref<NewAccountCredentialData>
+): Promise<boolean> {
   const notificationsStore = useNotificationsStore();
   if (!allFieldsArePopulated(credentials)) {
-    notificationsStore.setMessage(
-      "Account creation failed. Please ensure all fields are filled in.",
-      "error"
-    );
-    return;
+    notificationsStore.push("Error", "Please ensure all fields are filled in.");
+    return false;
   }
-  const countryID = convertCountry(
-    credentials.value.country.value as CountryName
-  ) as CountryID;
   const { data, error } = await useSupabaseAuthClient().auth.signUp({
-    email: credentials.value.email as string,
-    password: credentials.value.password as string,
+    email: credentials.value.email!,
+    password: credentials.value.password!,
     options: {
       data: {
-        preferred_name: credentials.value.preferred_name as string,
-        country_id: countryID,
-        locale: credentials.value.locale as string,
+        preferred_name: credentials.value.name,
+        country_id: credentials.value.country?.country_id,
+        locale: credentials.value.locale,
       },
     },
   });
   if (error) {
-    notificationsStore.setMessage(error.message, "error");
-    return;
+    notificationsStore.push("Error", error.message);
+    return false;
   }
   // Supabase doesn't notify if the email address is already registered
   // https://github.com/supabase/supabase-js/issues/296#issuecomment-1372552875
   if (data.user?.identities?.length === 0) {
-    notificationsStore.setMessage(
-      "Email address is already registered.",
-      "error"
-    );
-    return;
+    notificationsStore.push("Error", "Email address is already registered.");
+    return false;
   }
-  notificationsStore.setMessage("Account created successfully!", "success");
-  await navigateTo("/login");
+  notificationsStore.push("Success", "Account created successfully!");
+  return true;
 }
 /**
  * Attemps logout via SupabaseAuthClient (@nuxtjs/supabase).
- * Pushes a notification to the user and redirects to root page.
+ * Pushes a notification to the user to inform them whether the request was successful.
  */
 export async function logoutUser(): Promise<void> {
   const notificationsStore = useNotificationsStore();
   const userStore = useUserStore();
-  const request = await useSupabaseAuthClient().auth.getUser();
-  if (!request.data.user) {
-    notificationsStore.setMessage("No user currently logged in.", "error");
-    return;
-  }
   const { error } = await useSupabaseAuthClient().auth.signOut();
-  if (error) {
-    notificationsStore.setMessage(error.message, "error");
-    return;
-  }
-  userStore.clearData();
-  notificationsStore.setMessage("Logged out successfully!", "success");
-  await navigateTo("/");
+  if (error) throw new Error(error.message);
+  userStore.$reset();
+  notificationsStore.push("Success", "Logged out successfully!");
 }
