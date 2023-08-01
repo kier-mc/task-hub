7100d1ea8bfe824733c8acf1c1ce67c429087ed2
Kieran <kiermccarth+github@gmail.com>
2023-08-01 17:42:32 +0100

    Updated notification store with new interfaces

--- stores/notifications.ts
+++ stores/notifications.ts
@@ -1,18 +1,21 @@
+// Types
+import type { NotificationsStoreState } from "types/store.notifications";
+
 /**
  * A Pinia store that handles notifications.
  * Used for pushing messages from the system to notify the user of application state;
  * success, errors, warnings etc.
  */
 export const useNotificationsStore = defineStore("notifications", {
-  state: () => ({
-    message: null as string | null,
-    title: null as string | null,
+  state: (): NotificationsStoreState => ({
+    title: null,
+    message: null,
   }),
   actions: {
     /**
      * Pushes a message to the notification store.
-     * @param title {string} A title to help provide context to the message.
-     * @param message {string} A message to notify the user.
+     * @param title {string} A title that appears in the header of the toast component.
+     * @param message {string} A message which is displayed in the body of the toast component.
      */
     push(title: string, message: string): void {
       [this.title, this.message] = [null, null];
