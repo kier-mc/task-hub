3283a326c24e4220d33d3896211f7b2c0714ae79
Kieran <kiermccarth+github@gmail.com>
2023-08-10 15:42:50 +0100

    Updated with speed preferences

--- types/unions/generic.units.ts
+++ types/unions/generic.units.ts
@@ -1,2 +1,3 @@
 export type TemperatureUnitsShort = "c" | "k" | "f";
 export type TemperatureUnitsFull = "celsius" | "kelvin" | "fahrenheit";
+export type SpeedUnitsShort = "ms" | "kmh" | "mph" | "kn";
