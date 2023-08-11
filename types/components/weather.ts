27d3ecbc099845b8c4d0a4e7c24d6719c529a374
Kieran <kiermccarth+github@gmail.com>
2023-08-11 12:03:05 +0100

    Added new interface for weather panel component

---
+++ types/components/weather.ts
@@ -0,0 +1,15 @@
+export interface WeatherPanelData {
+  index: number;
+  set_1: {
+    label: string | number;
+    value: string | number;
+  };
+  set_2: {
+    label: string | number;
+    value: string | number;
+  };
+  set_3: {
+    label: string | number;
+    value: string | number;
+  };
+}
