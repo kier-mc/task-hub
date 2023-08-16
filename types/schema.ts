f166611df30c74d929865b35ec39b618de22732e
Kieran <kiermccarth+github@gmail.com>
2023-08-16 20:14:18 +0100

    Updated schema types to reflect database changes

--- types/schema.ts
+++ types/schema.ts
@@ -13,33 +13,59 @@ import type {
 } from "./unions/generic.units";
 
 export interface Database {
-  users: {
-    user_id: number;
-    user_uuid: string;
-    created_at: string;
-    email: string;
-    preferred_name: string;
-    country_id: CountryID;
-    locale: string;
-    preferences_app: DatabaseAppPreferences;
-    preferences_region: DatabaseRegionPreferences;
-    preferences_units: DatabaseUnitPreferences;
-  };
-  tasks: {
-    task_id: number;
-    created_at: string;
-    author_id: number;
-    task: string;
-    description: string;
-    frequency_id: FrequencyID;
-    edited_at: string;
-  };
-  frequency: {
-    frequency_id: FrequencyID;
-    repeats_every: FrequencyRepetition;
-  };
-  countries: CountryData;
+  users: UsersTable;
+  tasks: TasksTable;
+  frequency: FrequencyTable;
+  countries: CountriesTable;
+  tags: TagsTable;
 }
+
+export interface UsersTable {
+  user_id: number;
+  user_uuid: string;
+  created_at: string;
+  email: string;
+  preferred_name: string;
+  country_id: CountryID;
+  locale: string;
+  preferences_app: DatabaseAppPreferences;
+  preferences_region: DatabaseRegionPreferences;
+  preferences_units: DatabaseUnitPreferences;
+}
+
+export interface TasksTable {
+  task_id: number;
+  created_at: string;
+  author_id: number;
+  task: string;
+  description: string;
+  frequency_id: FrequencyID;
+  edited_at: string;
+  tag_id: number;
+}
+
+export interface FrequencyTable {
+  frequency_id: FrequencyID;
+  repeats_every: FrequencyRepetition;
+}
+
+export interface CountriesTable {
+  country_id: CountryID;
+  country_name: CountryName;
+  iso_code: CountryISOCode;
+}
+
+export interface TagsTable {
+  tag_id: number;
+  created_at: string;
+  tag: string;
+  type: string;
+}
+
+/**
+ * Need to resolve the multiple sources of truth present here for country data
+ * and find a better overall solution.
+ */
 
 export interface CountryData {
   country_id: CountryID;
