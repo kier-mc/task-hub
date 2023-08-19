76f4ed79352436f61a8f82d8875bcf9535f1e5fa
Kieran <kiermccarth+github@gmail.com>
2023-08-19 11:52:38 +0100

    Added tags as unions

---
+++ types/unions/schema.tags.ts
@@ -0,0 +1,18 @@
+export type TagID = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
+
+export type TagLabel =
+  | "personal"
+  | "home"
+  | "family"
+  | "work"
+  | "urgent"
+  | "high priority"
+  | "low priority"
+  | "health"
+  | "shopping"
+  | "finance"
+  | "study"
+  | "social"
+  | "travel";
+
+export type TagType = "context" | "priority" | "category";
