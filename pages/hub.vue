ab3fcf5477be3c6b3cbdab8b8b3339bc2a7e7e2b
Kieran <kiermccarth+github@gmail.com>
2023-09-08 14:08:47 +0100

    REFACTOR: Streamlined hub page

--- pages/hub.vue
+++ pages/hub.vue
@@ -1,75 +1,25 @@
 <template>
-  <div class="container-info">
-    <section class="info">
-      <ClientOnly>
-        <div class="info__datetime">
-          <div class="info__date">
-            <HubDate />
-          </div>
-          <div class="info__time">
-            <HubTime />
-          </div>
-        </div>
-      </ClientOnly>
-      <div class="info__weather">
-        <HubWeather />
-      </div>
-    </section>
-  </div>
-  <div class="temp">
-    <TaskCreate />
-  </div>
+  <section class="hub">
+    <HubSidebar class="hub__sidebar" />
+    <HubTasks class="hub__tasks" />
+  </section>
 </template>
 
 <style scoped lang="scss">
 @use "../assets/scss/data/colour";
 @use "../assets/scss/data/layout";
-.container-info {
-  position: relative;
-  background-image: colour.$hub-header-background;
-}
-.info {
+.hub {
   display: flex;
-  align-items: flex-end;
-  max-width: layout.$breakpoint-xl;
+  height: 100%;
   margin-inline: auto;
-  color: hsl(210, 5%, 90%);
-  &__datetime {
+  &__sidebar {
+    flex-basis: 30ch;
     min-width: 30ch;
-    border-right: 1px solid colour.$hub-header-border;
-  }
-  &__date,
-  &__time {
-    display: flex;
-    align-items: center;
-    text-rendering: optimizeLegibility;
-    font-variant: tabular-nums;
-  }
-  &__date {
-    height: 4rem;
-    padding-inline: 1rem;
-    border-bottom: 1px solid colour.$hub-header-border;
-  }
-  &__time {
-    height: 2rem;
-    padding-inline: 1rem;
-  }
-  &__weather {
-    flex-grow: 1;
-    margin-block: auto;
   }
 }
 </style>
 
 <script setup lang="ts">
 // Meta
-definePageMeta({ layout: "hub", middleware: "auth-only" });
-
-// Pinia stores
-const userStore = useUserStore();
-
-// Hooks
-onMounted(async () => {
-  if (!userStore.response) userStore.fetchData();
-});
+definePageMeta({ middleware: "auth-only" });
 </script>
