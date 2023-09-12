96cbb6e708bf968c86608434955c25b096716c88
Kieran <kiermccarth+github@gmail.com>
2023-09-12 15:21:56 +0100

    FIX: Tasks should now take up all remaining width in hub view

---
+++ components/HubTasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="tasks">
+    <TaskOptions />
+    <TaskList />
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/scss/data/layout";
+.tasks {
+  position: relative;
+  width: calc(100% - 1rem);
+  max-width: layout.$breakpoint-xl;
+  padding: 0.5rem;
+}
+</style>
+
+<script setup lang="ts"></script>
