69febc1e3597bcacb8b6bd0c22d96351009b155f
Kieran <kiermccarth+github@gmail.com>
2023-09-20 13:15:43 +0100

    REFACTOR: Complete rework of task list component

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,60 +1,20 @@
 <template>
-  <div class="view-task">
-    <div v-if="isLoading" class="view-task__loading">
-      <AppLoadingIndicator :options="propData.loadingIndicator" />
-    </div>
-    <template v-else>
-      <template v-if="!taskStore.taskCount()">No tasks found!</template>
-      <template v-else>
-        <div
-          class="view-task__tasks"
-          v-for="(task, index) in taskStore.tasks"
-          :key="index"
-        >
-          <TaskItem :taskData="task" />
-        </div>
-      </template>
-    </template>
-  </div>
+  <section class="task-list">
+    <section v-if="!props.data.length" class="task-list__empty">
+      No tasks found.
+    </section>
+    <section v-else class="task-list__list">
+      <TaskItem v-for="(task, index) in props.data" :data="task" :key="index" />
+    </section>
+  </section>
 </template>
 
-<style scoped lang="scss">
-.view-task {
-  &__loading {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  &__tasks {
-    &:not(:last-child) {
-      margin-bottom: 1.5rem;
-    }
-  }
-}
-</style>
+<style scoped lang="scss"></style>
 
 <script setup lang="ts">
-/* Pinia stores */
-const taskStore = useTaskStore();
-/* Prop data */
-const propData = {
-  loadingIndicator: {
-    type: "dots",
-    size: {
-      width: 48,
-      height: 48,
-    },
-    colour: {
-      hue: 0,
-      saturation: 0,
-      lightness: 100,
-    },
-  } as LoadingIndicatorData,
-};
-/* Reactive variables */
-const isLoading: Ref<boolean> = ref(true);
-onMounted(async () => {
-  if (!taskStore.taskCount()) await taskStore.getTasks();
-  isLoading.value = false;
+import type { TaskObject } from "~/types/components/tasks";
+
+const props = defineProps({
+  data: { type: Array as PropType<TaskObject[]>, required: true },
 });
 </script>
