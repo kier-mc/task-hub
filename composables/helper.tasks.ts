e7d2d7f1a443c38dbd55980b4c51db0ab61588cf
Kieran <kiermccarth+github@gmail.com>
2023-09-26 15:25:59 +0100

    UPDATE/REFACTOR: Update types, refactor tag search to linear O(N) method, refactor tag insert function

--- composables/helper.tasks.ts
+++ composables/helper.tasks.ts
@@ -1,17 +1,13 @@
 // Types
-import type { TagsTable, TagData } from "~/types/schema";
-import type {
-  FormTagPropData,
-  AutocompleteEmitFrequencyData,
-} from "~/types/components/forms";
+import type { TagData, TasksTagsJoinTable } from "~/types/schema";
+import type { AutocompleteEmitFrequencyData } from "~/types/components/forms";
 import type {
   FrequencyID,
   FrequencyRepetition,
 } from "~/types/unions/schema.frequency";
 import type { TagID, TagLabel, TagType } from "~/types/unions/schema.tags";
-import type { TaskObject } from "~/types/components/tasks";
 
-const TAG_DATA = <Omit<TagsTable, "created_at">[]>[
+const TAG_DATA: TagData[] = [
   {
     tag_id: 1,
     label: "low priority",
@@ -99,27 +95,6 @@ const FREQUENCY_TERMS = [
   "Yearly",
 ];
 
-/**
- * A baseline filter function, intended for extension via other functions.
- * Iterates through all tags associated with a task and returns true if
- * the predicate is found.
- * @param task {TaskObject}
- * The current task is that being queried.
- * @param predicate {TagID}
- * A number (matching a predefined TagID) to search for.
- * @returns {TaskObject | undefined}
- * Any task that matches the predicate, or undefined if no matches are found.
- */
-function tagFilter(task: TaskObject, predicate: TagID): TaskObject | undefined {
-  if (!task.tags) return;
-  for (let i = 0; i < task.tags.length; i++) {
-    if (Object.values(task.tags).includes(predicate)) {
-      return task;
-    }
-  }
-  return;
-}
-
 export const $tasks = {
   frequency: {
     generateAutocompleteData: (): AutocompleteEmitFrequencyData[] => {
@@ -156,31 +131,24 @@ export const $tasks = {
     },
   },
   tags: {
-    searchByID: (predicate: TagID): Omit<TagData, "created_at"> => {
-      const payload: Omit<TagData, "created_at"> = {
+    searchByID: (predicate: TagID): TagData => {
+      const payload: TagData = {
         tag_id: null,
         label: null,
         type: null,
       };
-      let start = 0;
-      let end = TAG_DATA.length - 1;
-      while (start <= end) {
-        const mid = Math.floor((start + end) / 2);
-        if (TAG_DATA[mid].tag_id === predicate) {
-          payload.tag_id = TAG_DATA[mid].tag_id;
-          payload.label = TAG_DATA[mid].label;
-          payload.type = TAG_DATA[mid].type;
-          return payload;
-        } else if (TAG_DATA[mid].tag_id < predicate) {
-          start = mid + 1;
-        } else {
-          end = mid - 1;
+      for (let i = 0; i < TAG_DATA.length; i++) {
+        const data = TAG_DATA[i];
+        if (data.tag_id === predicate) {
+          payload.tag_id = data.tag_id;
+          payload.label = data.label;
+          payload.type = data.type;
         }
       }
       return payload;
     },
-    searchByLabel: (predicate: TagLabel): Omit<TagData, "created_at"> => {
-      const payload: Omit<TagData, "created_at"> = {
+    searchByLabel: (predicate: TagLabel): TagData => {
+      const payload: TagData = {
         tag_id: null,
         label: null,
         type: null,
@@ -195,8 +163,8 @@ export const $tasks = {
       }
       return payload;
     },
-    searchByType: (predicate: TagType): Omit<TagData, "created_at"> => {
-      const payload: Omit<TagData, "created_at"> = {
+    searchByType: (predicate: TagType): TagData => {
+      const payload: TagData = {
         tag_id: null,
         label: null,
         type: null,
@@ -211,12 +179,11 @@ export const $tasks = {
       }
       return payload;
     },
-    getPropData: (): FormTagPropData[] => {
-      const payload: FormTagPropData[] = [];
+    getPropData: (): TagData[] => {
+      const payload: TagData[] = [];
       for (let i = 0; i < TAG_DATA.length; i++) {
         const tag = TAG_DATA[i];
         payload.push({
-          index: i,
           tag_id: tag.tag_id,
           label: tag.label,
           type: tag.type,
@@ -224,14 +191,17 @@ export const $tasks = {
       }
       return payload;
     },
-    prepareDataForDBInsert: (taskID: number, tags: FormTagPropData[]) => {
+    prepareDataForDBInsert: (
+      taskID: number,
+      tags: TagData[]
+    ): TasksTagsJoinTable[] => {
       const payload = [];
       for (let i = 0; i < tags.length; i++) {
         const tag = tags[i];
-        if (tag.label) {
+        if (tag.tag_id) {
           payload.push({
             task_id: taskID,
-            tag_id: $tasks.tags.searchByLabel(tag.label).tag_id,
+            tag_id: tag.tag_id,
           });
         }
       }
