c4ab07161fe2ca95d4384a895738b52ac5fca7df
Kieran <kiermccarth+github@gmail.com>
2023-09-29 12:45:02 +0100

    REFACTOR: Alter styles to avoid y overflow and update semantics

--- pages/login.vue
+++ pages/login.vue
@@ -1,31 +1,33 @@
 <template>
-  <section class="container__login">
-    <header class="header">
-      <h2 class="header__title">Login</h2>
-    </header>
-    <form class="login" @keyup.enter="loginWrapper(ref(credentials))">
-      <FormInput
-        class="login__input"
-        :data="propData.data.form[0]"
-        :is-disabled="state.is_disabled"
-        :aria-disabled="state.is_disabled"
-        v-model:emit-value="credentials.email"
-      />
-      <FormInput
-        class="login__input"
-        :data="propData.data.form[1]"
-        :is-disabled="state.is_disabled"
-        :aria-disabled="state.is_disabled"
-        v-model:emit-value="credentials.password"
-      />
-      <AppButton
-        class="login__button"
-        :data="propData.data.button"
-        :is-disabled="state.is_disabled"
-        :is-loading="state.is_loading"
-      />
-    </form>
-  </section>
+  <div class="container__login">
+    <article class="login">
+      <header class="header">
+        <h2 class="header__title">Login</h2>
+      </header>
+      <form class="form" @keyup.enter="loginWrapper(ref(credentials))">
+        <FormInput
+          class="form__input"
+          :data="propData.data.form[0]"
+          :is-disabled="state.is_disabled"
+          :aria-disabled="state.is_disabled"
+          v-model:emit-value="credentials.email"
+        />
+        <FormInput
+          class="form__input"
+          :data="propData.data.form[1]"
+          :is-disabled="state.is_disabled"
+          :aria-disabled="state.is_disabled"
+          v-model:emit-value="credentials.password"
+        />
+        <AppButton
+          class="form__button"
+          :data="propData.data.button"
+          :is-disabled="state.is_disabled"
+          :is-loading="state.is_loading"
+        />
+      </form>
+    </article>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -35,11 +37,13 @@
 .container__login {
   max-width: 40ch;
   margin-inline: auto;
-  margin-top: 2rem;
-  box-shadow: effect.$drop-shadow-md;
+  padding-top: 2rem;
   @media (max-width: layout.$breakpoint-md) {
     max-width: calc(100% - 2rem);
   }
+}
+.login {
+  box-shadow: effect.$drop-shadow-md;
 }
 .header {
   display: flex;
@@ -55,7 +59,7 @@
     font-weight: bold;
   }
 }
-.login {
+.form {
   position: relative;
   padding: 1rem;
   margin-inline: auto;
