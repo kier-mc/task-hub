310abb4050a1d10b8c43b3fe24bab00f1cf16eb6
Kieran <kiermccarth+github@gmail.com>
2023-10-04 10:38:56 +0100

    ADD: Added 'full' designations for speed units

--- types/unions/generic.units.ts
+++ types/unions/generic.units.ts
@@ -1,3 +1,4 @@
 export type TemperatureUnitsShort = "c" | "k" | "f";
 export type TemperatureUnitsFull = "celsius" | "kelvin" | "fahrenheit";
 export type SpeedUnitsShort = "ms" | "kmh" | "mph" | "kn";
+export type SpeedUnitsFull = "metres per second" | "kilometres per hour" | "miles per hour" | "knots"
