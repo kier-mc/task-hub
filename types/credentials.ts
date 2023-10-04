b548a67bbdf67d2e5d7338500c621fea23bba39e
Kieran <kiermccarth+github@gmail.com>
2023-10-04 17:00:33 +0100

    ADD: Expand credential types for settings updates

--- types/credentials.ts
+++ types/credentials.ts
@@ -1,4 +1,6 @@
 import { CountryData } from "~/types/schema";
+import { CountryName } from "./unions/schema.country";
+import { TemperatureUnitsShort, SpeedUnitsShort } from "./unions/generic.units";
 
 export interface LoginCredentialData {
   email: string | null;
@@ -13,3 +15,52 @@ export interface NewAccountCredentialData {
   location: CountryData | null;
   locale: string | null;
 }
+
+export interface PersonalPreferences {
+  name: string;
+}
+
+export type PersonalPreferencesData = {
+  [key in keyof PersonalPreferences]: PersonalPreferences[key] | null;
+}
+
+export interface WeatherPreferences{
+  locale: string;
+  country: CountryName;
+}
+
+export type WeatherPreferencesData = {
+  [key in keyof WeatherPreferences]: WeatherPreferences[key] | null;
+}
+
+export interface LocalisationPreferences {
+  country: string;
+}
+
+export type LocalisationPreferencesData = {
+  [key in keyof LocalisationPreferences]: LocalisationPreferences[key] | null;
+}
+
+export interface UnitPreferences {
+  temperature: TemperatureUnitsShort;
+  speed: SpeedUnitsShort;
+}
+
+export type UnitPreferencesData = {
+  [key in keyof UnitPreferences]: UnitPreferences[key] | null;
+}
+
+export interface UserPreferences {
+  personal: PersonalPreferences;
+  weather: WeatherPreferences;
+  localisation: LocalisationPreferences;
+  units: UnitPreferences;
+}
+
+export type UserPreferencesData = {
+  [key in keyof UserPreferences]: UserPreferences[key] extends Record<string, any>
+    ? {
+        [property in keyof UserPreferences[key]]: UserPreferences[key][property] | null;
+      }
+    : UserPreferences[key] | null;
+};
