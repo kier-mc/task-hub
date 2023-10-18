6bf09fde38cab9a116aebc0f5333173be83a30e3
Kieran <kiermccarth+github@gmail.com>
2023-10-18 11:59:02 +0100

    UPDATE: Migrate from deprecated c8 to v8

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,7 +2,7 @@ import { defineVitestConfig } from "nuxt-vitest/config";
 export default defineVitestConfig({
   test: {
     coverage: {
-      provider: "c8",
+      provider: "v8",
       reportsDirectory: "./__test__/coverage",
     },
   },
