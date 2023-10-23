8cba20c0ffb78ed6699f9306bd02b7af58381953
Kieran <kiermccarth+github@gmail.com>
2023-10-23 17:34:00 +0100

    UPDATE: Change to create/replace for convenience and remove on update to avoid unwanted copying of default metadata

--- db/triggers/trigger-sync-auth-to-public.sql
+++ db/triggers/trigger-sync-auth-to-public.sql
@@ -1,7 +1,7 @@
 /*
  * SQL trigger for sync_auth_to_public() function
  */
-CREATE TRIGGER trigger_sync_auth_to_public
-AFTER INSERT OR UPDATE ON auth.users
+CREATE OR REPLACE TRIGGER trigger_sync_auth_to_public
+AFTER INSERT ON auth.users
 FOR EACH ROW
 EXECUTE FUNCTION public.sync_auth_to_public();
