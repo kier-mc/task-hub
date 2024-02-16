9a970e8a708d29f3e29dac2f92bed4a3fddefb21
Kieran <kiermccarth+github@gmail.com>
2024-02-16 12:38:37 +0000

    UPDATE: Add Nuxt as default testing environment

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,9 +1,10 @@
-import { defineVitestConfig } from "nuxt-vitest/config";
+import { defineVitestConfig } from "@nuxt/test-utils/config";
 export default defineVitestConfig({
   test: {
     coverage: {
       provider: "v8",
       reportsDirectory: "./__test__/coverage",
     },
+    environment: "nuxt",
   },
 });
